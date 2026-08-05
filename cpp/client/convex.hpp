02e9cb53108c0383bad35631c630cc5872d675f0
Mike Cann <mike.cann@gmail.com>
2026-08-05 19:10:09 +0800

    Preserve C++ Live adapter error types

--- cpp/client/convex.hpp
+++ cpp/client/convex.hpp
@@ -14,7 +14,7 @@ struct Error : std::runtime_error { using std::runtime_error::runtime_error; };
 struct ProtocolError : Error { using Error::Error; };
 struct FunctionError : Error { Json data; std::vector<std::string> logs; FunctionError(std::string message, Json value, std::vector<std::string> lines) : Error(std::move(message)), data(std::move(value)), logs(std::move(lines)) {} };
 struct Result { Json value; std::vector<std::string> logs; };
-struct Update { Json value; std::vector<std::string> logs; std::string error; Json error_data; };
+struct Update { Json value; std::vector<std::string> logs; std::string error; Json error_data; std::string error_name; };
 class Subscription {
  public:
   ~Subscription();
